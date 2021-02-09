638b2479b700fb966887863f553b0671107f6a73
Kiel Sprague <aereisdin@gmail.com>
2021-02-09 09:59:01 -0800

    Added joke API and changed accordion styling to match index style.

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
     var visited =localStorage.getItem("visited");
     if(visited === null){visited = 0}
-    
+    var punchline = document.querySelector(".punchline");
+    var setup = document.querySelector(".setup");
     function welcomeBack(){
         $("#welcomeBackModal").foundation("toggle")
     }
@@ -19,5 +20,14 @@ $(document).ready(function() {
        visited++;
         localStorage.setItem("visited", visited)
         })
-   
+    $(".joke").click( function() {
+        $.get( "https://official-joke-api.appspot.com/random_joke", function( data ) {
+            console.log(data);
+            console.log(data.setup)
+            question = data.setup;
+            setup.textContent = question;
+            punchline.textContent = data.punchline;
+            punchline.attr("style", "visibility:hidden")
+          });});
+    
 });
